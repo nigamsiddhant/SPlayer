e55508ec38dd713c1c560b028a8062526bcc863d
siddhant nigam <nigamsiddhant@gmail.com>
2021-02-03 12:49:10 +0530

    new

--- Package.swift
+++ Package.swift
@@ -18,14 +18,8 @@ let package = Package(
 //        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "6.0.0")),
 //    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(name: "MyFirstSPM", dependencies: ["Kingfisher", "Alamofire"])
         .target(
             name: "SPlayer",
-            dependencies: []),
-//        .testTarget(
-//            name: "MyFirstSPMTests",
-//            dependencies: ["MyFirstSPM"]),
+            dependencies: [])
     ]
 )
